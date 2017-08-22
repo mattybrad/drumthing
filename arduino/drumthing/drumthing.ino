ef0ffbb90728447b54a995ec4b20d5a5fdbe4f8a
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-08-22 22:24:50 +0100

    nice start, sounds good

---
+++ arduino/drumthing/drumthing.ino
@@ -0,0 +1,77 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthNoisePink      pink1;          //xy=78,321
+AudioMixer4              mixer1;         //xy=218,318
+AudioFilterStateVariable filter2;        //xy=365,318
+AudioFilterStateVariable filter3;        //xy=365,382
+AudioFilterStateVariable filter1;        //xy=366,239
+AudioEffectEnvelope      envelope2;      //xy=514,307
+AudioEffectEnvelope      envelope3;      //xy=515,360
+AudioEffectEnvelope      envelope1;      //xy=517,258
+AudioMixer4              mixer2;         //xy=665,322
+AudioOutputI2S           i2s1;           //xy=801,321
+AudioConnection          patchCord1(pink1, 0, mixer1, 0);
+AudioConnection          patchCord2(mixer1, 0, filter1, 0);
+AudioConnection          patchCord3(mixer1, 0, filter2, 0);
+AudioConnection          patchCord4(mixer1, 0, filter3, 0);
+AudioConnection          patchCord5(filter2, 1, envelope2, 0);
+AudioConnection          patchCord6(filter3, 2, envelope3, 0);
+AudioConnection          patchCord7(filter1, 0, envelope1, 0);
+AudioConnection          patchCord8(envelope2, 0, mixer2, 1);
+AudioConnection          patchCord9(envelope3, 0, mixer2, 2);
+AudioConnection          patchCord10(envelope1, 0, mixer2, 0);
+AudioConnection          patchCord11(mixer2, 0, i2s1, 0);
+AudioConnection          patchCord12(mixer2, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=281,34
+// GUItool: end automatically generated code
+
+
+void setup() {
+  // put your setup code here, to run once:
+  AudioMemory(50);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(0.5);
+  pink1.amplitude(0.6);
+  filter1.frequency(300);
+  filter2.frequency(800);
+  filter3.frequency(500);
+  envelope1.sustain(0.3);
+  envelope2.sustain(0.3);
+  envelope3.sustain(0.3);
+  envelope1.release(2000);
+  envelope2.release(1000);
+  envelope3.release(500);
+  mixer2.gain(2,0.5);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switch(random(4)) {
+    case 0:
+    envelope1.noteOn();
+    delay(50);
+    envelope1.noteOff();
+    break;
+    
+    case 1:
+    envelope2.noteOn();
+    delay(50);
+    envelope2.noteOff();
+    break;
+
+    case 2:
+    envelope3.noteOn();
+    delay(50);
+    envelope3.noteOff();
+    break;
+
+    default:
+    delay(50);
+  }
+  delay(120);
+}
